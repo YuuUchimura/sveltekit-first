a560e320d86edaaf38f74e544c9a4b7fb9b57574
Yuu <anpanmamZ3624@gmail.com>
2023-08-14 07:31:23 +0900

    🏗️ ssr, csr の設定方法

--- src/routes/user/+page.server.ts
+++ src/routes/user/+page.server.ts
@@ -4,7 +4,10 @@ type User = {
 	name: string;
 };
 
-export const prerender = true; //Prerenderingの設定
+// export const ssr = true;
+
+// export const csr = false;
+
 export const load = (async ({ fetch }) => {
 	const response = await fetch('https://jsonplaceholder.typicode.com/users');
 	const users: User[] = await response.json();
--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -4,6 +4,10 @@
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>ユーザ詳細画面</title>
+</svelte:head>
+
 <h1>ユーザ一覧</h1>
 <ul>
 	{#each data.users as user}
